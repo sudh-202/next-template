2034185cc864e3e676ecf888e1cedf5b9aed30fc
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-26 16:00:04 +0530

    the unusal error solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import clsx from "clsx";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
